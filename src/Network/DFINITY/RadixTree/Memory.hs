ec7037a0b8bef18d9dc1633fb1c98709407e5108
Enzo Haussecker <enzo@sovereign.io>
2018-05-10 12:28:42 -0700

    Keep storage API consistent with LeveLDB

--- src/Network/DFINITY/RadixTree/Memory.hs
+++ src/Network/DFINITY/RadixTree/Memory.hs
@@ -4,16 +4,18 @@ module Network.DFINITY.RadixTree.Memory
    ) where
 
 import Control.DeepSeq (force)
+import Control.Monad.IO.Class (MonadIO)
 import Data.ByteString (ByteString)
 import Data.ByteString.Short (ShortByteString, toShort)
 import Data.LruCache as LRU (LruCache, insert, lookup)
 import Database.LevelDB (DB, defaultReadOptions, defaultWriteOptions, get, put)
 
 load
-   :: LruCache ShortByteString ByteString
+   :: MonadIO m
+   => LruCache ShortByteString ByteString
    -> DB
    -> ByteString
-   -> IO (Maybe ByteString)
+   -> m (Maybe ByteString)
 load cache database key =
    case fst <$> LRU.lookup short cache of
       Nothing -> get database defaultReadOptions key
@@ -21,11 +23,12 @@ load cache database key =
    where short = toShort key
 
 store
-   :: LruCache ShortByteString ByteString
+   :: MonadIO m
+   => LruCache ShortByteString ByteString
    -> DB
    -> ByteString
    -> ByteString
-   -> IO (LruCache ShortByteString ByteString)
+   -> m (LruCache ShortByteString ByteString)
 store cache database key value = do
    put database defaultWriteOptions key value
    pure $ force $ insert short value cache
