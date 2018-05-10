ba3a1f8b2aa442758ccbe9fa78942111f8b44db1
Enzo Haussecker <enzo@sovereign.io>
2018-05-09 18:36:04 -0700

    Expose byte to bit array conversions

--- src/Network/DFINITY/RadixTree/Bits.hs
+++ src/Network/DFINITY/RadixTree/Bits.hs
@@ -1,6 +1,8 @@
 module Network.DFINITY.RadixTree.Bits
    ( fromBits
+   , fromByte
    , toBits
+   , toByte
    ) where
 
 import Data.Bits ((.|.), setBit, testBit)
@@ -13,11 +15,11 @@ fromBits = pack . go where
    go [] = []
    go xs = let (a, b) = splitAt 8 xs in toByte a : go b
 
-toBits :: ByteString -> [Bool]
-toBits = concatMap fromByte . unpack
-
 fromByte :: Word8 -> [Bool]
 fromByte = flip map order . testBit
+
+toBits :: ByteString -> [Bool]
+toBits = concatMap fromByte . unpack
 
 toByte :: [Bool] -> Word8
 toByte = foldl (.|.) 0 . zipWith (bool 0 . setBit 0) order
