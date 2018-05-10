f1796ee951a428d1ad8071f3854e008c4a70d19b
Enzo Haussecker <enzo@sovereign.io>
2018-05-09 17:53:16 -0700

    Implement lookup function

--- src/Network/DFINITY/RadixTree.hs
+++ src/Network/DFINITY/RadixTree.hs
@@ -1,5 +1,33 @@
-module Network.DFINITY.RadixTree where
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RecordWildCards #-}
 
-import Codec.Serialise ()
-import Database.LevelDB ()
+module Network.DFINITY.RadixTree
+   ( Network.DFINITY.RadixTree.lookup
+   ) where
 
+import Codec.Serialise (deserialise)
+import Data.Bool (bool)
+import Data.ByteString (ByteString)
+import Data.ByteString.Lazy.Char8 (fromStrict)
+
+import Network.DFINITY.RadixTree.Bits
+import Network.DFINITY.RadixTree.Memory
+import Network.DFINITY.RadixTree.Types
+
+lookup :: RadixTree -> ByteString -> IO (Maybe ByteString)
+lookup tree = go tree . toBits
+   where
+   go RadixTree {..} = \ case
+      [] -> pure _radixLeaf
+      test:residue ->
+         case bool _radixLeft _radixRight test of
+            Nothing -> pure Nothing
+            Just key -> do
+               result <- load _radixCache _radixDatabase key
+               case result of
+                  Nothing -> pure Nothing
+                  Just value -> do
+                     let root = deserialise $ fromStrict value
+                     flip go residue $ RadixTree _radixCache _radixDatabase root
+      where
+      RadixBranch {..} = _radixRoot
