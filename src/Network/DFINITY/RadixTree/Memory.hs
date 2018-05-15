20a7d7b1cc015ebf4533bd34b326c633a0eab6f3
Enzo Haussecker <enzo@sovereign.io>
2018-05-15 16:57:37 -0700

    Move serialization into memory module

--- src/Network/DFINITY/RadixTree/Memory.hs
+++ src/Network/DFINITY/RadixTree/Memory.hs
@@ -1,12 +1,17 @@
+{-# LANGUAGE TupleSections #-}
+
 module Network.DFINITY.RadixTree.Memory
    ( free
    , load
    , store
    ) where
 
+import Codec.Serialise (deserialise, serialise)
 import Control.DeepSeq (force)
 import Control.Monad.IO.Class (MonadIO)
-import Data.ByteString (ByteString)
+import Crypto.Hash.SHA256 (hash)
+import Data.ByteString.Char8 as Byte (ByteString, take)
+import Data.ByteString.Lazy (fromStrict, toStrict)
 import Data.ByteString.Short (toShort)
 import Data.LruCache as LRU (insert, lookup)
 import Database.LevelDB (DB, defaultReadOptions, defaultWriteOptions, delete, get, put)
@@ -16,15 +21,20 @@ import Network.DFINITY.RadixTree.Types
 free :: MonadIO m => DB -> ByteString -> m ()
 free database = delete database defaultWriteOptions
 
-load :: MonadIO m => RadixCache -> DB -> ByteString -> m (Maybe ByteString)
-load cache database key =
-   case fst <$> LRU.lookup short cache of
-      Nothing -> get database defaultReadOptions key
-      Just value -> pure $ Just value
-   where short = toShort key
+load :: MonadIO m => RadixCache -> DB -> ByteString -> m (Maybe RadixBranch)
+load cache database root = do
+   result <- case fst <$> LRU.lookup short cache of
+      Nothing -> get database defaultReadOptions root
+      Just bytes -> pure $ Just bytes
+   pure $ deserialise . fromStrict <$> result
+   where
+   short = toShort root
 
-store :: MonadIO m => RadixCache -> DB -> ByteString -> ByteString -> m RadixCache
-store cache database key value = do
-   put database defaultWriteOptions key value
-   pure $ force $ insert short value cache
-   where short = toShort key
+store :: MonadIO m => RadixCache -> DB -> RadixBranch -> m (RadixCache, ByteString)
+store cache database branch = do
+   put database defaultWriteOptions root bytes
+   pure $ (,root) $ force $ insert short bytes cache
+   where
+   bytes = toStrict $ serialise branch
+   root  = Byte.take 20 $ hash bytes
+   short = toShort root
