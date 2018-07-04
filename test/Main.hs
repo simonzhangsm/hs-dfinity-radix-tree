7c005214ad25d16c52e10e26af68735f98ed57c5
Enzo Haussecker <enzo@sovereign.io>
2018-07-04 07:44:27 +0200

    Remove LevelDB dependency from unit tests

--- test/Main.hs
+++ test/Main.hs
@@ -8,19 +8,20 @@
 
 module Main where
 
-import Control.Monad (foldM_, mzero)
+import Control.Arrow (first)
+import Control.Monad (foldM_, mzero, void)
 import Control.Monad.IO.Class (MonadIO(..))
-import Control.Monad.Trans.Resource (MonadResource, runResourceT)
+import Control.Monad.State (StateT, runStateT)
 import Data.Aeson (FromJSON(..), Object, Value(..), eitherDecode)
 import Data.ByteString.Base16 (decode, encode)
 import Data.ByteString.Char8 (ByteString, unpack)
 import Data.ByteString.Lazy.Char8 as Lazy (readFile)
 import Data.ByteString.Short (fromShort)
 import Data.Default.Class (Default(..))
-import Data.HashMap.Strict as Map (elems, lookup)
+import Data.HashMap.Strict as HashMap (elems, lookup)
+import Data.Map.Strict (Map, empty)
 import Data.Text as Text (Text, drop)
 import Data.Text.Encoding (encodeUtf8)
-import Database.LevelDB (DB, Options(..))
 import System.Console.CmdArgs (Data, cmdArgs)
 
 import Network.DFINITY.RadixTree
@@ -28,12 +29,11 @@ import Network.DFINITY.RadixTree
 data Args
    = Args
    { json :: FilePath
-   , path :: FilePath
    , test :: String
    } deriving Data
 
 instance Default Args where
-   def = Args "test/tests.json" "test/testdb" "*"
+   def = Args "test/tests.json" "*"
 
 data Op
    = Insert ByteString ByteString
@@ -54,11 +54,9 @@ instance Show Op where
       Merkleize value -> "Merkleize" ++ pretty value
       where pretty = mappend " 0x" . unpack . encode
 
-parse
-   :: Object
-   -> Maybe Op
+parse :: Object -> Maybe Op
 parse object = do
-   op <- Map.lookup "op" object
+   op <- HashMap.lookup "op" object
    case op of
       "set" -> Insert <$> get "key" object <*> get "value" object
       "delete" -> Delete <$> get "key" object
@@ -66,21 +64,14 @@ parse object = do
       "stateRoot" -> Merkleize <$> get "value" object
       _ -> Nothing
 
-get
-   :: Text
-   -> Object
-   -> Maybe ByteString
+get :: Text -> Object -> Maybe ByteString
 get key object = do
-   value <- Map.lookup key object
+   value <- HashMap.lookup key object
    case value of
       String text -> Just $ fst $ decode $ encodeUtf8 $ Text.drop 2 text
       _ -> Nothing
 
-step
-   :: MonadResource m
-   => RadixTree DB
-   -> Op
-   -> m (RadixTree DB)
+step :: RadixTree () -> Op -> StateT (Map ByteString ByteString) IO (RadixTree ())
 step tree op = do
    liftIO $ print op
    case op of
@@ -97,24 +88,20 @@ step tree op = do
          case result of
             Just (value', tree') | value == value' -> pure tree'
             Just (value', _) -> throw
-               [ "Expecting value "
-               , ", but received value "
-               , " for key "
-               ] [value, value', key]
+               ["Expecting value ", ", but received value ", " for key "]
+               [value, value', key]
             Nothing -> throw
-               [ "Expecting value "
-               , ", but received no value for key "
-               ] [value, key]
+               ["Expecting value ", ", but received no value for key "]
+               [value, key]
       Merkleize value -> do
-         (fromShort -> value', tree') <- merkleizeRadixTree tree
+         (value', tree') <- first fromShort <$> merkleizeRadixTree tree
          if value == value'
          then pure tree'
          else throw
-            [ "Expecting state root "
-            , ", but received state root "
-            ] [value, value']
-      where
-      throw err = fail . concat . zipWith mappend err . map show
+            ["Expecting state root ", ", but received state root "]
+            [value, value']
+   where
+   throw err = fail . concat . zipWith mappend err . map show
 
 main :: IO ()
 main = do
@@ -122,11 +109,10 @@ main = do
    contents <- Lazy.readFile json
    case eitherDecode contents of
       Left err -> fail err
-      Right vctors -> runResourceT $ do
-         let options = def {createIfMissing = True}
-         tree <- createRadixTree 262144 2048 Nothing (path, options)
+      Right vctors -> void $ flip runStateT empty $ do
+         tree <- createRadixTree 262144 2048 Nothing ()
          if test == "*"
          then foldM_ step tree `mapM_` elems vctors
-         else case Map.lookup test vctors of
-            Nothing -> fail $ "unknown test vector: " ++ test
+         else case HashMap.lookup test vctors of
+            Nothing -> fail $ "Unknown test vector " ++ show test
             Just ops -> foldM_ step tree $ concat [ops]
